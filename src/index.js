9a9eae8eb4354971d750e4be5a5e41f7751a6121
Jorge <jomontes97@hotmail.com>
2022-10-03 16:30:25 -0500

    Adding new partials and footer

--- src/index.js
+++ src/index.js
@@ -23,6 +23,6 @@ app.set('view engine', 'ejs')
 app.use(indexRoutes)
 
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
-console.log('Server is listening on port', 3000);
+console.log('Server is listening on port', process.env.PORT || 3000);
