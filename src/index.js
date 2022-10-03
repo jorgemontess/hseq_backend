abdd2a572dae9e8b57cf3ea289d795b2b21abc98
jorge <jomontes97@hotmail.com>
2022-10-03 00:34:34 -0700

    Creating and adding views

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 //Importando express, para hacerlo de esta manera toca crear en el package.json un "type":"module";
 import express from "express";
+import {dirname, join} from 'path'
+import {fileURLToPath} from 'url'
 
-//Creando objeto app para llamar express
-const app = express();
 
-//URL
-//Peticion http: Metodo GET para que cuando soliciten url se muestre algo
-app.get('/', (req, res) => res.send("HSEQ CONTROL TOTAL"))
+const app = express()
 
-//Llamando el metodo listen y colocando el puerto 3000
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+//console.log(join(__dirname, 'views'))
+
+
+app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+//ROUTES
+app.get('/', (req, res) => res.render('index'))
+
+app.get('/servicios', (req, res) => res.render('servicios'))
+
+app.get('/contactenos', (req, res) => res.render('contactenos'))
+
 app.listen(3000);
 
 console.log('Server is listening on port', 3000);
