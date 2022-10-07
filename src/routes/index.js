fe4d161eb6e0319febebd7205b7a93a23dd2c074
Jorge <jomontes97@hotmail.com>
2022-10-07 14:29:43 -0500

    Agregando comentarios

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,17 @@
 import {Router} from 'express'
 const router = Router()
 
+// Creating the routes for the application
+// Using the partials methods
 
+
+// Route for the index
 router.get('/', (req, res) => res.render('index'))
 
+// Route for the services
 router.get('/servicios', (req, res) => res.render('servicios'))
 
+// Route for the contacts
 router.get('/contactenos', (req, res) => res.render('contactenos'))
 
 export default router
